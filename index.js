d7fe473232b21d95981fe34e33ac629ca2157ae0
Hasibul294 <hasibulhasan294@gmail.com>
2022-01-10 21:20:39 +0600

    get and post api complete

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
-// const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
 const port = 5000;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "testdb",
+});
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("This is server of technext");
+app.get("/addEmployee", (req, res) => {
+  const sqlSelect = "SELECT * FROM employee_info;";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/addEmployee", (req, res) => {
+  const info = req.body;
+  const fastName = info.fastName;
+  const lastName = info.lastName;
+  const sqlInsert =
+    "INSERT INTO employee_info (fastName, lastName) VALUES (?,?);";
+  db.query(sqlInsert, [fastName, lastName], (err, result) => {
+    res.json(result);
+  });
 });
 
 app.listen(port, () => {
